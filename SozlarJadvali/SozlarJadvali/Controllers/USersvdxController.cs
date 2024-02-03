e8d8f46d648dfa92855bdfcb5afab1e8536cd813
Axliddin Xushboqov <xushboqovaxliddin7@gmail.com>
2024-02-03 13:18:49 +0500

    adhvauidhiu

---
+++ SozlarJadvali/SozlarJadvali/Controllers/USersvdxController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using RESTFulSense.Controllers;
+using SozlarJadvali.Models.Words;
+using SozlarJadvali.Services.Words;
+
+namespace SozlarJadvali.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class USersvdxController : RESTFulController
+    {
+        private readonly IWordService wordService;
+
+        public USersvdxController(IWordService wordService)
+        {
+            this.wordService = wordService;
+        }
+
+        [HttpGet("Get-All-sdvsdvs")]
+        [EnableQuery]
+        [EnableCors("AllowSpecificOrigin")]
+        public ActionResult<Word> GetAllWords()
+        {
+            IQueryable<Word> AllWords =
+                this.wordService.RetrieveAllWords();
+
+            return Ok(AllWords);
+        }
+    }
+}
